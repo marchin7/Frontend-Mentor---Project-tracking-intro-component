1e72b3de8fb4e747abd71b9ee6566c0b00bdffeb
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-01-22 17:48:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const nav = document.getElementById('nav');
+const menuBtns = document.getElementById('menu-btns');
+const hamburger = document.getElementById('hamburger');
+const closeBtn = document.getElementById('close-menu-btn');
+
+menuBtns.addEventListener('click', ()=>{
+    nav.classList.toggle('active');
+
+    if(nav.classList.contains('active')){
+        hamburger.classList.add('active');
+        closeBtn.classList.add('active');
+    }
+    else {
+        hamburger.classList.remove('active');
+        closeBtn.classList.remove('active');
+    }
+});
+
+
